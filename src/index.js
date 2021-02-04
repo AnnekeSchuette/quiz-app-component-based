87baa37c33fd645c571cae49098304561e73adad
Anneke Schütte <a.schuette.hh@gmail.com>
2021-02-04 12:48:40 +0100

    Add Button component to storybook

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Button from './components/Button'
 
 const button = Button('Wohoo!')
+const button2 = Button('Hell yeah, Motherfucker!')
 
 document.body.append(button)
+document.body.append(button2)
