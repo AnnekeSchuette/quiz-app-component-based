c0541f076c743e0d09e9e0f385820ea366a5483f
Anneke Schütte <a.schuette.hh@gmail.com>
2021-02-05 09:21:11 +0100

    Change createElement function to add children, update Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,23 @@ import createElement from '../lib/createElement'
 import './Header.css'
 
 export default function Header(title, subtitle) {
-  const subtitleEl = createElement('span', {
-    className: 'Header__subtitle',
-    innerText: subtitle,
-  })
-
-  const titleEl = createElement('h1', {
-    className: 'Header__title',
-    innerText: title,
-    innerHTML: subtitleEl,
-  })
-
-  const el = createElement('header', {
-    className: 'Header',
-  })
-
-  el.append(titleEl)
+  const el = createElement(
+    'header',
+    {
+      className: 'Header',
+    },
+    createElement(
+      'h1',
+      {
+        className: 'Header__title',
+        innerText: title,
+      },
+      createElement('span', {
+        className: 'Header__subtitle',
+        innerText: subtitle,
+      })
+    )
+  )
 
   return el
 }
